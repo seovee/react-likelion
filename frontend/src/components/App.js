bbdb4d11c492f150778b69396533a7ed178f0cd9
seovee <jinseob102@naver.com>
2023-02-16 09:10:02 +0900

    리액트 3일차

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,30 +1,28 @@
 class App extends React.Component {
-  
   state = {
-    headline: 'React Application',
+    headline: "React Application",
     // isDisabled: false,
     isToggle: false,
     isLoading: false,
   };
 
   originalHeadline = this.state.headline;
-  willUpdateHeadline = 'NEW HEADLINE! 😃';
+  willUpdateHeadline = "NEW HEADLINE! 😃";
 
   handleChangeHeadline = () => {
-
-    let assignHeadlineContent = '';
+    let assignHeadlineContent = "";
 
     // 조건 처리
     // 문을 사용할 것인가?
     if (this.state.isToggle) {
       this.setState({
         isToggle: false,
-        headline: this.originalHeadline
+        headline: this.originalHeadline,
       });
     } else {
       this.setState({
         isToggle: true,
-        headline: this.willUpdateHeadline
+        headline: this.willUpdateHeadline,
       });
     }
 
@@ -34,14 +32,13 @@ class App extends React.Component {
     //   headline: 'NEW HEADLINE! 😃',
     //   isDisabled: true,
     // });
-  }
+  };
 
   render() {
-
     const { isToggle, headline } = this.state;
 
     if (this.state.isLoading) {
-      return <div role="alert">데이터 로딩 중...</div> 
+      return <div role="alert">데이터 로딩 중...</div>;
     }
 
     return (
@@ -49,10 +46,10 @@ class App extends React.Component {
         <h1>{headline}</h1>
         <button
           // disabled={this.state.isDisabled}
-          type="button" 
+          type="button"
           onClick={this.handleChangeHeadline}
         >
-          { isToggle ? '오리지널 헤드라인으로 변경' : '뉴 헤드라인으로 변경' }
+          {isToggle ? "오리지널 헤드라인으로 변경" : "뉴 헤드라인으로 변경"}
         </button>
       </div>
     );
